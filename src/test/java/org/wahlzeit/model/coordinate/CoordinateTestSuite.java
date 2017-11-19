df876f207ee4982e74dea800c21c7bf5bbc90769
Artur Mandybura <artur.mandybura@googlemail.com>
2017-11-19 21:39:30 +0100

    fixed some comments

--- src/test/java/org/wahlzeit/model/coordinate/CoordinateTestSuite.java
+++ src/test/java/org/wahlzeit/model/coordinate/CoordinateTestSuite.java
@@ -1,3 +1,27 @@
+/*Author: Artur Mandybura, https://github.com/ArtyMandy/wahlzeit
+ *
+ * Class: CoordinateTestSuite
+ *
+ *Version information: adap-cw06
+ *
+ * Date: 19. Nov. 2017
+ *
+ * This file is part of the Wahlzeit photo rating application.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
 package org.wahlzeit.model.coordinate;
 
 import org.junit.runner.RunWith;
